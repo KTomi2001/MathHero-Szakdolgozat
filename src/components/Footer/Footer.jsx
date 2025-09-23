8e2bd9a2bdc67afc040d696d54ecf9831059379c
Keresztes Tamás <keresztes.tamas2001@icloud.com>
2025-09-24 00:17:02 +0200

    Reszponzivítás

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -24,55 +24,6 @@ const Footer = () => {
   return (
     <footer className="mathhero-footer">
       <div className="container">
-        <div className="row footer-sections">
-          <div className="col-md-4">
-            <div className="footer-logo">
-              <span>MATHHERO</span>
-            </div>
-            <p className="footer-tagline">Sosem volt könnyebb!</p>
-          </div>
-          <div className="col-md-2">
-            <h5 className="footer-title">Oldalak</h5>
-            <ul className="footer-links">
-              <li>
-                <Link to="/">Főoldal</Link>
-              </li>
-              <li>
-                <a href="#" onClick={handleOpenRegister}>Regisztráció</a>
-              </li>
-              <li>
-                <a href="#" onClick={handleOpenLogin}>Bejelentkezés</a>
-              </li>
-            </ul>
-          </div>
-          <div className="col-md-3">
-            <h5 className="footer-title">Funkciók</h5>
-            <ul className="footer-links">
-              <li>
-                <a>Játékos feladatok</a>
-              </li>
-              <li>
-                <a>Személyre szabott feladatok</a>
-              </li>
-              <li>
-                <a>Fejlődés nyomon követése</a>
-              </li>
-              <li>
-                <a>Ranglista</a>
-              </li>
-            </ul>
-          </div>
-          <div className="col-md-3">
-            <h5 className="footer-title">Kapcsolat</h5>
-            <ul className="footer-links">
-              <li>
-                <a href="mailto:info@mathhero.hu">info@mathhero.hu</a>
-              </li>
-              <li>+36 12 345 6789</li>
-            </ul>
-          </div>
-        </div>
-        <div className="row">
           <div className="col-12">
             <div className="footer-bottom">
               <p className="text-center w-100">
@@ -80,7 +31,6 @@ const Footer = () => {
               </p>
             </div>
           </div>
-        </div>
       </div>
 
       <Authentication 
